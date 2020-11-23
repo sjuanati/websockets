4fc11d56fdda442586f2a206c8ad6a1e6a8765d0
Sergi Juanati <wint3rmute@gmail.com>
2020-11-23 19:30:39 +0100

    sending msg client <--> server

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,35 @@
-console.log('hello world');
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+const moment = require('moment');
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+const wsServer = new webSocketServer({
+    httpServer: server
+});
+
+// All active connections in this object
+//const clients = {};
+let connection;
+
+wsServer.on('request', (request: any) => {
+    console.log(`${moment().format()} Connection established from ${request.origin}`);
+    // You can rewrite this part of the code to accept only the requests from allowed origin
+    const connection = request.accept(null, request.origin);
+
+    // User sent a message
+    connection.on('message', function (message: any) {
+        console.log('Message received: ', message);
+        connection.sendUTF(JSON.stringify('holas manolas'));
+    });
+
+    // user disconnected
+    connection.on('close', (connection: any) =>  {
+        console.log((new Date()) + " Peer disconnected.");
+    });
+
+});
+
+
+
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ws = new W3CWebSocket('ws://127.0.0.1:8000');
+
+const App = () => {
+	const [message, setMessage] = React.useState<string>('');
+	
+	React.useEffect(() => {
+		
+		ws.onopen = () => {
+			console.log('WebSocket Client Connected');
+			ws.onmessage = (msg: any) => {
+				const dataFromServer = JSON.parse(msg.data)
+				console.log('msg: ', msg);
+				console.log('msg parsed: ', dataFromServer);
+			};
+		};
+		return () => ws.close();
+	}, []);
+
+	const handleSend = () => {
+		ws.send(JSON.stringify({
+			publicId: message,
+		}));
+	};
+
+	return (
+		<div className={'App'}>
+			<div>
+				Practical Intro To WebSockets
+			</div>
+			<input
+				onChange={(elem) => setMessage(elem.target.value)}>
+			</input>
+			<button
+				onClick={handleSend}>
+				Send
+			</button>
+			<div>
+				{message}
+			</div>
+		</div>
+
+
+	);
 }
 
 export default App;
