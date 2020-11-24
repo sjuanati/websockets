090760bafe52a2e47fa142ce548e1160bf1184cf
Sergi Juanati <wint3rmute@gmail.com>
2020-11-24 11:31:23 +0100

    Send to me, send to all

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,33 +2,72 @@ const webSocketsServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
 const moment = require('moment');
-// Spinning the http server and the websocket server.
+
+// Start http & websocket servers
 const server = http.createServer();
 server.listen(webSocketsServerPort);
 const wsServer = new webSocketServer({
     httpServer: server
 });
 
-// All active connections in this object
-//const clients = {};
-let connection;
+// To manage all active connections
+const users = new Map();
+let connection: any;
 
 wsServer.on('request', (request: any) => {
     console.log(`${moment().format()} Connection established from ${request.origin}`);
-    // You can rewrite this part of the code to accept only the requests from allowed origin
-    const connection = request.accept(null, request.origin);
+    connection = request.accept(null, request.origin); // TODO: only accept allowed origin
+    let id: string = '';
 
     // User sent a message
-    connection.on('message', function (message: any) {
-        console.log('Message received: ', message);
-        connection.sendUTF(JSON.stringify('holas manolas'));
+    connection.on('message', function (msg: any) {
+        try {
+            if (msg.type === 'utf8') {
+
+                // Show incoming message
+                const { publicId, action, message } = JSON.parse(msg.utf8Data);
+                id = publicId;
+                console.log(`Message received`);
+
+                // Store connection message
+                if (action === 'ping') {
+                    users.set(publicId, connection);
+                };
+
+                // Send data only to user passes number 5
+                if (action === 'send to me') {
+                    (users.get(publicId))
+                        ? users.get(publicId).sendUTF(JSON.stringify({
+                            field: 'only you!',
+                        }))
+                        : null;
+                };
+
+                // Send to all users
+                if (action === 'send to all') {
+                    for (let conn of users.values()) {
+                        conn.sendUTF(JSON.stringify({
+                            field: 'Global communication',
+                        }));
+                    }
+                }
+            };
+
+        } catch (err) {
+            console.log('Error in wsServer->onMessage: ', err);
+        };
     });
 
     // user disconnected
-    connection.on('close', (connection: any) =>  {
-        console.log((new Date()) + " Peer disconnected.");
-    });
+    connection.on('close', () => {
+        try {
+            users.delete(id);
+            console.log('Peer disconnected');
+        } catch (err) {
+            console.log('Error in wsServer->onClose: ', err);
+        };
 
+    });
 });
 
 
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -6,9 +6,10 @@ const ws = new W3CWebSocket('ws://127.0.0.1:8000');
 
 const App = () => {
 	const [message, setMessage] = React.useState<string>('');
-	
+	const [publicId, setPublicId] = React.useState<string>('');
+
 	React.useEffect(() => {
-		
+
 		ws.onopen = () => {
 			console.log('WebSocket Client Connected');
 			ws.onmessage = (msg: any) => {
@@ -16,34 +17,91 @@ const App = () => {
 				console.log('msg: ', msg);
 				console.log('msg parsed: ', dataFromServer);
 			};
+
+			// Send ID number;
+			ws.send(JSON.stringify({
+				publicId: Math.floor(Math.random() * 1000),
+				action: 'ping',
+				message: 'Hello',
+			}));
 		};
 		return () => ws.close();
 	}, []);
 
-	const handleSend = () => {
+	const handleSendID = () => {
+		(publicId)
+			? ws.send(JSON.stringify({
+				publicId: publicId,
+				action: 'ping',
+				message: '',
+			}))
+			: console.log('Fill in ID first');
+	};
+
+	const handleSendData = () => {
 		ws.send(JSON.stringify({
-			publicId: message,
+			publicId: publicId,
+			action: 'send to me',
+			message: message,
+		}));
+	};
+
+	const handleSendDataAll = () => {
+		ws.send(JSON.stringify({
+			publicId: '',
+			action: 'send to all',
+			message: message,
 		}));
 	};
 
 	return (
-		<div className={'App'}>
-			<div>
-				Practical Intro To WebSockets
+		<div className={'container'}>
+			<div className={'header'}>
+				WebSockets tests
 			</div>
-			<input
-				onChange={(elem) => setMessage(elem.target.value)}>
-			</input>
-			<button
-				onClick={handleSend}>
-				Send
-			</button>
-			<div>
-				{message}
+			<div className={'item'}>
+				<div className={'description'}>
+					<button
+						className={'grey'}
+						onClick={handleSendID}
+					>
+						Send ID
+                    </button>
+				</div>
+				<div>
+					<input
+						onChange={(elem) => setPublicId(elem.target.value)}
+					/>
+				</div>
 			</div>
+
+			<div className={'item'}>
+				<div className={'description'}>
+					<button
+						className={'grey'}
+						onClick={handleSendData}>
+						Send data to ME
+                    </button>
+				</div>
+				<div>
+					<input
+						onChange={(elem) => setMessage(elem.target.value)}
+					/>
+				</div>
+			</div>
+
+			<div className={'item'}>
+				<div className={'description'}>
+					<button
+						className={'grey'}
+						onClick={handleSendDataAll}>
+						Send data to ALL
+                    </button>
+				</div>
+			</div>
+
+
 		</div>
-
-
 	);
 }
 
